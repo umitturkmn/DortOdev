1a941a68a033166cd0131f16370536a5cd2d41a1
umit482 <umit.482@gmail.com>
2022-02-16 12:46:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DortOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Tools yeni = new();
+         Console.WriteLine("Ödev Projesine Hoşgeldiniz...");
+         Console.WriteLine("-----------------------------");
+         Console.WriteLine("Ödev Listesi: ");
+         Console.WriteLine("1. Ödev : 1 ");
+         Console.WriteLine("2. Ödev : 2 ");
+         Console.WriteLine("3. Ödev : 3 ");
+         Console.WriteLine("4. Ödev : 4 ");
+         Console.WriteLine("Çıkmak için : X ");
+         
+        while (true)
+        {
+            Console.Write("Görmek İstediğiniz ödevin numarasını tuşlayın: ");
+            string secim = Console.ReadLine();
+            switch (secim)
+         {
+             case  "1":
+             Console.WriteLine("Ödev 1 : Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.");
+                yeni.CiftYazdır();
+                break;
+            case  "2":
+                Console.WriteLine("Ödev 2 : Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.");
+                yeni.EsitveBolunur();
+                break;
+            case  "3":
+                Console.WriteLine("Ödev 3 : Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.");
+                yeni.KelimeTersten();
+                break;
+            case  "4":
+                Console.WriteLine("Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.");
+                yeni.HarfveKelime();
+                break;
+            case "X":
+            case "x":
+                Environment.Exit(0);
+                break;
+             default:
+                Console.WriteLine("Hatalı tuşladınız tekrar deneyin.");
+                break;
+         }
+        }
+        
+         
+
+        }   
+    }
+}
---
+++ Tools.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DortOdev
+{
+    public class Tools
+    {
+        public void CiftYazdır()
+        {
+            Console.WriteLine("Kaç Adet Sayı Girmek İstiyorsunuz: ");
+            int giris = int.Parse(Console.ReadLine());
+            int[] dizi = new int[giris];
+            for (int i = 0; i <= giris-1; i++)
+            {
+                Console.Write("{0}. Sayıyı giriniz: ",i+1);
+                int sayi = int.Parse(Console.ReadLine());
+                if (sayi % 2 == 0)
+                {
+                    dizi[i] = sayi;
+                    
+                }
+            }
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Girdiğiniz Sayılar Arasından Çift Olanlar: ");
+            foreach (var item in dizi)
+            {
+                if (item != 0)
+                {
+                    Console.Write(item + "-");
+                    
+                } 
+            }
+            Console.Write(" ");
+            Console.WriteLine("-------------------------------------------");
+            
+        }
+        
+        public void EsitveBolunur()
+        {
+            Console.WriteLine("-------------------------------------------");
+            Console.Write("Bir sayı girinizi: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Bir sayı daha giriniz: ");
+            int m = int.Parse(Console.ReadLine());
+            int[] dizi = new int[999];
+            for (int i = 0; i <n; i++)
+            {
+                Console.Write("{0}. Sayıyı giriniz: ",i+1);
+                int sayi = int.Parse(Console.ReadLine());
+                if (sayi == m || sayi % m == 0)
+                {
+                    dizi[i] = sayi;
+                }
+
+            }
+            Console.WriteLine("-------------------------------------------");
+            foreach (var item in dizi)
+            {
+                if (item > 0)
+                {
+                    Console.Write(item + "-");
+                    
+                } 
+            }
+            Console.Write(" ");
+            Console.WriteLine("-------------------------------------------");
+        }
+        public void KelimeTersten()
+        {
+            Console.Write("Bir sayı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+            string[] dizi = new string[sayi];
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.Write("{0}. kelimeyi giriniz: ",i+1);
+                dizi[i] = Console.ReadLine().ToString();
+            }
+            Console.Write(" ");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Girdiğiniz kelimelerin en sondan en ilke sıralanışı: ");
+            Array.Reverse(dizi);
+            foreach (var item in dizi)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("-------------------------------------------");
+        }
+        public void HarfveKelime()
+        {
+            Console.Write("Bir Cümle Giriniz: ");
+            string cumle = Console.ReadLine().ToString();
+            int sayac = 1;
+            foreach (char item in cumle)
+            {
+                if(item != ' ')
+                {
+                    sayac++;
+                }
+            }
+            string[] kelimeler = cumle.Split(' ');
+            Console.WriteLine("Girdiğiniz Cümle Toplam " + kelimeler.Length + " kelimden oluşmaktadır.");
+            Console.WriteLine("Aynı zamanda toplam harf sayısı: " + sayac );
+
+
+
+        }
+          
+    }
+}
